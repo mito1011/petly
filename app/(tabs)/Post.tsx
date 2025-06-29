74fdca38c36bac05f1998290c6f5e87f129a1500
Akay Kayihan <akaykayihan@hotmail.de>
2025-06-29 22:09:05 +0200

    fix: Update image URLs in imageMap for consistency and accuracy; modify todo item for clarity

--- app/(tabs)/Post.tsx
+++ app/(tabs)/Post.tsx
@@ -13,7 +13,7 @@ import {
 const imageMap = {
   Dogs: 'https://placedog.net/400/400?id=10',
   Cats: 'https://images.pexels.com/photos/1276553/pexels-photo-1276553.jpeg',
-  Other: 'https://images.pexels.com/photos/52570/rabbit-pet-easter-animal-52570.jpeg',
+  Other: 'https://www.shutterstock.com/image-photo/row-five-common-small-domestic-260nw-394830724.jpg',
 };
 
 const animalTypeOptions = ['Dogs', 'Cats', 'Other'];
--- data/dummyURL.ts
+++ data/dummyURL.ts
@@ -1,5 +1,5 @@
 const imageMap = {
   Dogs: 'https://placedog.net/400/400?id=10',
   Cats: 'https://images.pexels.com/photos/1276553/pexels-photo-1276553.jpeg',
-  Other: 'https://images.pexels.com/https://www.shutterstock.com/image-photo/rabbit-guinea-pig-natural-green-600nw-2469425103.jpg/52570/rabbit-pet-easter-animal-52570.jpeg',
+  Other: 'https://www.shutterstock.com/image-photo/row-five-common-small-domestic-260nw-394830724.jpg',
 };
